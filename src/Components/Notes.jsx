36f49bc480281921db1e874273fa6650a53793cf
Kelvin Akaba <akabakelvin@gmail.com>
2023-06-26 20:27:16 +0000

    styling

--- src/Components/Notes.jsx
+++ src/Components/Notes.jsx
@@ -21,10 +21,12 @@ function Notes() {
                     <div className='border-2 border-indigo-600 p-6 mt-5' key={index} item={item}>
                         <div className='font-bold text-red-400 text-3xl'>{item.title}</div>
                         <div>{item.description.slice(0, 100)}</div>
-                        <Link style={{textDecoration: 'none'}} to={`/notedetails/${item.id}`}>
-                            <button className='bg-sky-300 mt-5 text-black hover:border-l-green-500'>View full note</button>
-                        </Link>
-                            <button onClick={() => handleDelete(item.id)} className='bg-red-500 ms-5'>Delete Note</button>
+                            <Link style={{textDecoration: 'none'}} to={`/notedetails/${item.id}`}>
+                                <button className='bg-sky-300 mt-5 text-black hover:border-l-green-500'>View full note</button>
+                            </Link>
+                            <div className='p-3'>
+                                <button onClick={() => handleDelete(item.id)} className='bg-red-500'>Delete Note</button>
+                            </div>
                         
                     </div>
                 ))
